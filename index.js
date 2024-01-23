de15b8c11aada65413a016eff960e71f46394c6b
Anas0055 <manaskhan969@gmail.com>
2024-01-23 02:12:25 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+"use strict";
+// // Storing a person's name with whitespace characters
+// let personName: string = "\t\t John Doe \n";
+// // Printing the name with leading and trailing whitespace
+// console.log("Name with whitespace:");
+// console.log("----");
+// console.log(personName);
+// console.log("----");
+// // Stripping the whitespace using trim() function and printing the name without whitespace
+// let strippedName: string = personName.trim();
+// console.log("\nName after stripping whitespace:");
+// console.log("----");
+// console.log(strippedName);
+// console.log("----");
+// Stripping Names: Store a person’s name, and include some whitespace characters 
+// at the beginning and end of the name. Make sure you use each
+// character combination, "\t" and "\n", at least once.
+//  Print the name once, so the whitespace around the name is displayed.
+//  Then print the name after striping the white spaces.
+let personName = "\t\t ali khan \n";
+console.log("name with wighespace");
+console.log("---");
+console.log(personName);
+console.log("---");
+console.log("name whit out whitespace");
+console.log(personName.trim());
---
+++ index.ts
@@ -0,0 +1,13 @@
+// Stripping Names: Store a person’s name, and include some whitespace characters 
+// at the beginning and end of the name. Make sure you use each
+// character combination, "\t" and "\n", at least once.
+//  Print the name once, so the whitespace around the name is displayed.
+//  Then print the name after striping the white spaces.
+  let personName:string= "\t\t ali khan \n";
+  console.log ("name with wighespace");
+  console.log("---");
+  console.log(personName);
+  console.log("---")
+console.log("name whit out whitespace");
+console.log(personName.trim());
+
